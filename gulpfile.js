099aebd1fecaa70e0e1f6e9e41436c81c6431999
Vladimir Mit <sonic.tcm@gmail.com>
2019-07-23 16:21:44 +0300

    update package

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	autoprefixer = require('gulp-autoprefixer'),
 	cssnano = require('gulp-cssnano'),
+	uglify = require('gulp-uglify'),
 	sass = require('gulp-sass'),
 	watch = require('gulp-watch'),
-	sourcemaps = require('gulp-sourcemaps');
+	sourcemaps = require('gulp-sourcemaps'),
+	clean = require('gulp-clean');
 
 var browserSync = require('browser-sync').create();
 
@@ -18,6 +20,7 @@ var appDir = 'app/',
 	dist_cssDir = distDir + 'css/',
 	dist_jsDir = distDir + 'js/';
 
+
 //запуск локального сервера
 gulp.task('server', function() {
 	browserSync.init({
@@ -25,9 +28,8 @@ gulp.task('server', function() {
 			baseDir: 'app'
 		}
 	});
-	gulp.watch(app_scssDir + '**/*.scss', gulp.series('sass'));
-	gulp.watch(appDir + 'css/*.css').on('change', browserSync.reload);
-	gulp.watch(appDir + 'js/*.js').on('change', browserSync.reload);
+	gulp.watch(app_scssDir + '**/*.scss', gulp.series('sass', 'delComplCssFile', 'concatCss'));
+	gulp.watch(app_jsDir + '*.js').on('change', browserSync.reload);
 	gulp.watch(appDir + '*.html').on('change', browserSync.reload);
 });
 
@@ -80,19 +82,14 @@ gulp.task('copyBalloonScss', function() {
         .pipe(gulp.dest(app_scssDir));
 });
 
-//default task
-gulp.task('default', gulp.series('server'));
-
-
-/* Задачи для Dist */
 //объединение файлов CSS
 gulp.task('concatCss', function() {
 	return gulp.src([app_cssDir + '*.css'])
 		.pipe(sourcemaps.init())
-			.pipe(concat('styles.css'))
+			.pipe(concat('all.css'))
 			.pipe(cssnano())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(dist_cssDir));
+		.pipe(gulp.dest(app_cssDir));
 });
 
 //объединение файлов Js
@@ -100,32 +97,62 @@ gulp.task('concatJs', function() {
 	return gulp.src([app_jsDir + '*.js'])
 		.pipe(sourcemaps.init())
 			.pipe(concat('all.js'))
+			.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(dist_jsDir));
+		.pipe(gulp.dest(app_jsDir));
+});
+
+//удаление all.css
+gulp.task('delComplCssFile', function () {
+	return gulp.src(app_cssDir + 'all.css', {allowEmpty: true})
+	  .pipe(clean());
+});
+
+//удаление all.js
+gulp.task('delComplJsFile', function () {
+	return gulp.src(app_jsDir + 'all.js', {allowEmpty: true})
+	  .pipe(clean());
+});
+
+//default task
+gulp.task('default', gulp.series('sass', 'concatJs', 'server'));
+
+
+/* Задачи для Dist */
+//удаление все внутри Dist
+gulp.task('delDist', function () {
+	return gulp.src(distDir + '*', {read: false})
+	  .pipe(clean());
 });
 
 //копируем все шрифты
-gulp.task('copyFonts', function () {
+gulp.task('copyFontsDist', function () {
 	return gulp.src(appDir + 'fonts/**/*')
 		.pipe(gulp.dest(distDir + 'fonts/'));
 });
 
 //копируем все изображения
-gulp.task('copyImgs', function () {
+gulp.task('copyImgsDist', function () {
 	return gulp.src(appDir + 'img/**/*')
 		.pipe(gulp.dest(distDir + 'img/'));
 });
 
+//копируем all.css
+gulp.task('copyCssDist', function () {
+	return gulp.src(app_cssDir + 'all.css')
+		.pipe(gulp.dest(dist_cssDir));
+});
+
+//копируем all.js
+gulp.task('copyJsDist', function () {
+	return gulp.src(app_jsDir + 'all.js')
+		.pipe(gulp.dest(dist_jsDir));
+});
+
 //копируем все HTML
-gulp.task('copyHTML', function () {
+gulp.task('copyHTMLDist', function () {
 	return gulp.src(appDir + '*.html')
 		.pipe(gulp.dest(distDir));
 });
 
-gulp.task('toDist', function () {
-	gulp.parallel('copyHTML');
-	gulp.parallel('copyImgs');
-	gulp.parallel('copyFonts');
-	gulp.parallel('concatJs');
-	gulp.parallel('concatCss');
-});
+gulp.task('toDist', gulp.series('copyHTMLDist', 'copyJsDist', 'copyCssDist', 'copyImgsDist', 'copyFontsDist'));
